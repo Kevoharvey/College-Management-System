c003828959130e63c328a96fd9bc81638447e69d
Kevoharvey$ <Kevin.Harvey2006@gmail.com>
2026-05-08 00:03:23 +0300

    Initial commit

---
+++ algorithms.java
@@ -0,0 +1,48 @@
+public class algorithms {
+    // ---------------------------------------
+    // 1, Binary Search Algorithm
+    // ---------------------------------------
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    // ---------------------------------------
+    // 2, Bubble Sort Algorithm
+    // ---------------------------------------
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // Optimization: If no swap occurred, array is sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
